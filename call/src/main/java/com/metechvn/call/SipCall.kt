0f4d6142924aa94770d016ee0d8213254c49a0be
caunb <nguyenbacau163@gmail.com>
2021-05-11 14:21:18 +0700

    fix sipcall

--- call/src/main/java/com/metechvn/call/SipCall.kt
+++ call/src/main/java/com/metechvn/call/SipCall.kt
@@ -7,30 +7,23 @@ import com.metechvn.call.service.LinphoneService
 import org.linphone.core.*
 
 class SipCall {
-    private val core: Core? = LinphoneService.getCore()
-
-    fun configCore(activity: Activity) {
-        activity.startService(Intent(activity, LinphoneService::class.java))
-    }
 
     fun login(username: String, password: String, domain: String) {
-        var mAccountCreator = LinphoneService.getCore()?.createAccountCreator(null)
+        val mAccountCreator = LinphoneService.getCore()?.createAccountCreator(null)
         mAccountCreator?.username = username
         mAccountCreator?.password = password
         mAccountCreator?.domain = domain
         mAccountCreator?.transport = TransportType.Tcp
-        val cfg: ProxyConfig = mAccountCreator!!.createProxyConfig()
+        val cfg: ProxyConfig? = mAccountCreator?.createProxyConfig()
         LinphoneService.getCore()?.defaultProxyConfig = cfg
     }
 
     fun call(number: String) {
-        if (core != null) {
-            val addressToCall: Address? = core.interpretUrl(number)
-            val params: CallParams = core.createCallParams(null)
-            params.enableAudio(true)
+            val addressToCall: Address? =  LinphoneService.getCore()?.interpretUrl(number)
+            val params: CallParams? =  LinphoneService.getCore()?.createCallParams(null)
+            params?.enableAudio(true)
             if (addressToCall != null)
-                core.inviteAddressWithParams(addressToCall, params)
-        }
+                LinphoneService.getCore()?.inviteAddressWithParams(addressToCall, params)
     }
 
 }
